7ae87f7f73ca910e144b6a46ee6ac3917053c684
juancalle19 <jesteban.2741@gmail.com>
2023-02-28 15:40:30 -0500

    Category controller Post  Get

---
+++ Sales.Shared/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres")]
+        [Display(Name = "Categoria")]
+        public string Name { get; set; } = null!;
+    }
+}
